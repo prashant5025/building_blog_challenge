826069c0688e046baf80cbedf0c9afcad0396a22
Prashant Singh <psingh5025615@gmail.com>
2022-11-25 04:59:36 +0530

    initial commit

--- Job_API/controllers/jobs.js
+++ Job_API/controllers/jobs.js
@@ -1,14 +1,24 @@
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
+const { BadRequestError, NotFoundError } = require('../errors');
 const Job = require('../models/Job');
 
 
 const getAllJobs = async (req, res) => {
-    res.send('Get all jobs');
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('-createdAt')
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length})
 }
 // @desc Get a single job
 const getJob = async (req, res) => {
-    res.send('Get all jobs');
+    const {user:{userId}, params:{id:jobId}} = req
+    
+    const job = await Job.findOne({
+        _id: jobId,
+        createdBy: userId
+    })
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 
 //@desc Create a job
@@ -18,10 +28,35 @@ const createJob = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ job })
   }
 const updateJob = async (req, res) => {
-    res.send('Get all jobs');
+    const {body:{company,position},user:{userId}, params:{id:jobId}} = req
+    
+    if(company === '' || position === ''){
+        throw new BadRequestError('Please fill in all fields')
+    }
+    const job = await Job.findOneAndUpdate({
+        _id: jobId,
+        createdBy: userId
+    }, req.body, {
+        new: true,
+        runValidators: true
+
+    })
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 const deleteJob = async (req, res) => {
-    res.send('Get all jobs');
+    const {user: {userId}, params:{id:jobId}} = req
+
+    const job = await Job.findOneAndDelete({
+        _id: jobId,
+        createdBy: userId
+    })
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ msg: 'Job deleted', jobId:job._id})
 }
 
 module.exports = {
